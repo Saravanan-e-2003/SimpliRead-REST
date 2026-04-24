90e057b3775625cbb0c0e1a0beb0869ac4826a08
Saravanan-e-2003 <motts2003@gmail.com>
2026-04-24 23:26:51 +0530

    basic boilerplate setup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-//gotta start 
+import express from "express";
+import dotenv from "dotenv";
+import uploadRoutes from "./routes/uploadRoutes.js";
+
+dotenv.config();
+
+const app = express();
+
+// app.use("/api", uploadRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
